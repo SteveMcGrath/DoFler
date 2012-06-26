758666e1b966e533588f78f3518d709bc222026c
Steven McGrath <smcgrath@tenable.com>
2012-06-26 10:26:02 -0500

    Added documentation to the api client code.

--- dofler/api/client.py
+++ dofler/api/client.py
@@ -12,19 +12,43 @@ uploader = urllib2.build_opener(urllib2.HTTPCookieProcessor(cj),
                                 MultipartPostHandler.MultipartPostHandler())
 
 def login():
+    '''Login api.  Sends the appropriate information to get the signed cookie
+    we need to be able to post to the API.
+    '''
+
+    # Lets start off and pre-populate the information we know we need.
     data = {
         'salt': time.time(),
         'sensor': config.get('Settings', 'sensor_name'),
     }
+
+    # Also lets pull the information we need from the config file.
     secret = config.get('Settings', 'sensor_secret')
+
+    # Now we will hash everything together into one nice happy hex digest and
+    # then add it to the post data :)
     md5sum = md5()
     md5sum.update(data['sensor'], data['salt'], secret)
     data['auth'] = md5sum.hexdigest()
+
+    # Then lets send everything along to the API so that we can get the cookie
+    # we need.
     response = opener.open('%s/login' % config.get('Settings', 'dofler_address'), 
                            urlencode(data))
 
 
 def account(username, password, info, proto, parser):
+    '''account api call.  parses and uploads the account information specified
+    to the dofler server.
+    '''
+
+    # First thing, if the password is more than 3 characters in length, then
+    # we will obfuscate it.  We don't want to be completely brutal to the people
+    # we just pwned ;)
+    if len(password) >= 3:
+        password = '%s%s' % (password[:3], '*' * len(password) - 3)
+
+    # Building the post data
     data = {
         'username': username,
         'password': password,
@@ -32,10 +56,15 @@ def account(username, password, info, proto, parser):
         'proto': proto,
         'parser': parser,
     }
+
+    # And submitting the data to the dofler server.
     opener.open('%s/api/post/account' % config.get('Settings', 'dofler_address'),
                 urlencode(data))
 
 
 def image(filename):
+    '''image api call.  sends the image filename to the dofler server to be
+    parsed and added to the database.
+    '''
     uploader.open('%s/api/post/image' % config.get('Settings', 'dofler_address'),
                   {'file': open(filename)})
