728704879029c2ecf274e244bdc2dc2e7f35ce11
Steven McGrath <smcgrath@tenable.com>
2012-06-27 10:31:23 -0500

    Minor adjustments needed to make TCPXtract module work.

--- dofler/monitor/tcpxtract.py
+++ dofler/monitor/tcpxtract.py
@@ -4,12 +4,13 @@ import os
 import dofler.api.client
 
 class Parser(BaseParser):
-	rimage = re.compile(r'exporting to (.*?)$')
+    rimage = re.compile(r'exporting to (.*?)[\r]{0,1}$')
     cmd = 'tcpxtract -d {INTERFACE} -o /tmp'
+    stanza = 'TCPXtract'
 
     def parse(self, line):
-        files = self.rimage.findall(line):
+        files = self.rimage.findall(line)
         if len(files) > 0:
             filename = files[0]
-            dofler.api.client.image(filename)
+            dofler.api.client.image(filename.strip('\r'))
             os.remove(filename)
