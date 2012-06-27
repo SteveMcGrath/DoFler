87d7bdb9e0f89c60ab67acce0768cc7579ddddf8
Steven McGrath <smcgrath@tenable.com>
2012-06-27 10:31:45 -0500

    Removed config parsing and setup for multiprocess.

--- dofler/svc.py
+++ dofler/svc.py
@@ -9,23 +9,17 @@ def client():
     if config.getboolean('Ettercap', 'run'):
         print 'Starting Ettercap Monitor...'
         ettercap = monitor.ettercap.Parser()
-        ettercap.run(config.get('Ettercap', 'interface'),
-                     conifg.getint('Ettercap', 'timer'),
-                     config.getboolean('Ettercap', 'promisc'))
+        ettercap.start()
 
     if config.getboolean('TShark-http', 'run'):
         print 'Starting TShark-http monitor'
         tshark = monitor.tshark.Parser()
-        tshark.run(config.get('TShark-http', 'interface'),
-                   conifg.getint('TShark-http', 'timer'),
-                   config.getboolean('TShark-http', 'promisc'))
+        tshark.start()
 
     if config.getboolean('TCPXtract', 'run'):
         print 'Starting Driftnet monitor...'
         tcpxtract = monitor.tcpxtract.Parser()
-        tcpxtract.run(config.get('TCPXtract', 'interface'),
-                      conifg.getint('TCPXtract', 'timer'),
-                      config.getboolean('TCPXtract', 'promisc'))
+        tcpxtract.start()
 
     print 'Running.'
 
