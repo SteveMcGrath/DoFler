5744907aa8e52258942612bde5b6a8d0eeb74436
Steven McGrath <smcgrath@tenable.com>
2012-06-27 10:30:58 -0500

    Minor adjustments needed to make the ettercap module work.

--- dofler/monitor/ettercap.py
+++ dofler/monitor/ettercap.py
@@ -3,24 +3,29 @@ import re
 import dofler.api.client
 
 class Parser(BaseParser):
-	'''The Ettercap password parser.'''
-	ruser = re.compile(r'USER: (.*?)  ')	# USER Regex: Pulls out USER field.
-	rpass = re.compile(r'PASS: (.*?)  ')	# PASS Regex: Pulls out PASS field.
-	rinfo = re.compile(r'INFO: (.*?)$')		# INFO Regex: Pulls out INFO field.
-	promisc = {True: '', False: '-p'}		# Promiscuous flags.
-	cmd = 'ettercap {PROMISC} -Tzuqi {INTERFACE}'	# Command to run.
+    '''The Ettercap password parser.'''
+    ruser = re.compile(r'USER: (.*?)  ')    # USER Regex: Pulls out USER field.
+    rpass = re.compile(r'PASS: (.*?)  ')    # PASS Regex: Pulls out PASS field.
+    rinfo = re.compile(r'INFO: (.*?)$')     # INFO Regex: Pulls out INFO field.
+    rproto = re.compile(r'^(\w*) : ')       # PROTO Regex: Pulls the protocal.
+    promisc = {True: '', False: '-p'}       # Promiscuous flags.
+    cmd = 'ettercap {PROMISC} -Tzuqi {INTERFACE}'   # Command to run.
+    stanza = 'Ettercap'
 
-	def parse(self, line):
-		'''Ettercap line output parser.'''
-		if 'USER' in line:
+    def parse(self, line):
+        '''Ettercap line output parser.'''
+        if 'USER' in line:
 
-			usernames = self.ruser.findall(line)
-			passwords = self.rpass.findall(line)
-			infos = self.rinfo.findall(line)
+            usernames = self.ruser.findall(line)
+            passwords = self.rpass.findall(line)
+            infos = self.rinfo.findall(line)
+            protos = self.rproto.findall(line)
 
-			if len(usernames) > 0 and len(passwords) > 0:
-				username = usernames[0]
-				password = passwords[0]
-				info = infos[0]
-				dofler.api.client.account(username, passowrd, 
-										  info, proto, 'ettercap')		
+            if len(usernames) > 0 and len(passwords) > 0:
+                username = usernames[0]
+                password = passwords[0]
+                info = infos[0]
+                proto = protos[0]
+                if proto is not 'HTTP':
+                    dofler.api.client.account(username, password, 
+                                              info, proto, 'ettercap')
