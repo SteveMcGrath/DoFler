9b6cb94b81e9b656c7d2203023527db124962188
Steven McGrath <smcgrath@tenable.com>
2012-06-27 18:16:02 -0500

    Updates to setup.py

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ import sys
 
 setup(
     name='DoFler',
-    version='0.0.1a',
+    version='0.0.2a',
     description='Dashboard of Fail - A distibuted Wall of Sheep',
     author='Steven McGrath',
     author_email='steve@chigeek.com',
     url='https://github.com/SteveMcGrath/DoFler',
-    packages = ['dofler']
-    entry_points = {
+    packages=['dofler', 'dofler.monitor', 'dofler.api'],
+    entry_points={
         'console_scripts': [
             'dofler-client = dofler.svc:client',
             'dofler-server = dofler.svc:server',
@@ -25,10 +25,12 @@ setup(
         'pil',
     ],
     data_files=[
-        ('/etc', 'dofler.config.sample'),
+        ('/etc', ['dofler.config']),
+        ('/etc/init', ['init-scripts/dofler-client.conf', 
+                       'init-scripts/dofler-client.conf']),
         ('/usr/share/dofler', ['static/jquery.js', 'static/live-view.js',
                                'static/style.css', 'static/viewer.html'])
-    ]
+    ],
     classifiers=[
         'Development Status :: 2 - Pre-Alpha',
         'Intended Audience :: Information Technology',
