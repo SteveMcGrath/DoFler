59b6adcd8a8dc372acf79aa92324a4bbe46db90c
Steven McGrath <smcgrath@tenable.com>
2012-06-27 18:13:24 -0500

    Changed to support general local use.

--- static/live-view.js
+++ static/live-view.js
@@ -7,30 +7,33 @@ accounts();
 window.setInterval(images, 5000);
 window.setInterval(accounts, 5000);
 
+var urlhost = ''
+var numimages = 25
+var numaccounts = 25
 
 var hashes = [];
 function images(){
-	$.getJSON('http://10.10.50.120:8089/api/images/25', function(data) {	
+	$.getJSON(urlhost + '/api/images/' + numimages, function(data) {	
 		$.each(data, function(key, val) {
 		     if(hashes.indexOf(val.hash) == -1){
-			     $("#actual-images").prepend('<img class="resize" src="http://10.10.50.120:8089/api/image/' + val.hash + '" />');
+			     $("#actual-images").prepend('<img class="resize" src="' + urlhost + '/api/image/' + val.hash + '" />');
 			     hashes.push(val.hash)
 			 }
         });
-        hashes = Array.prototype.slice.call(hashes, -6);
+        hashes = Array.prototype.slice.call(hashes, -numimages);
 
 	});
 };
 var ids = [];
 function accounts(){
-	$.getJSON('http://10.10.50.120:8089/api/accounts/25', function(data) {	
+	$.getJSON(urlhost + '/api/accounts/' + numaccounts, function(data) {	
 		$.each(data, function(key, val) {
 			     if(ids.indexOf(val.id) == -1){
 			         $("#accounts-table tbody").prepend("<tr><td>"+ val.info +"</td><td>"+ val.proto +"</td><td>"+ val.username +"</td><td>"+ val.password +"</td></tr>");
 			     ids.push(val.id)
 			 }
 		});
-		ids = Array.prototype.slice.call(ids, -6);
+		ids = Array.prototype.slice.call(ids, -numaccounts);
 	});
 };
 
