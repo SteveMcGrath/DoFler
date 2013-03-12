7599b9ae078c6873d122e787e43f222d2d6cb346
Steven McGrath <smcgrath@tenable.com>
2013-03-12 12:06:13 -0500

    Yay a startup func!

---
+++ client/dofler/__init__.py
@@ -0,0 +1,16 @@
+import api
+import config
+import log
+import monitor
+
+def startup():
+    pids = []
+    c = config.config
+    parsers = monitor.get_parsers()
+    for parser in parsers:
+        stanza = 'Parser: ' % parser
+        if stanza in c.sections() and c.getboolean(stanza, 'enabled'):
+            pid = parsers[parser]()
+            pid.start()
+            pids.append(pid)
+    
