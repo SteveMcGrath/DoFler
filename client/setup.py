7ea35d095b7e0f0c4eead682c45db85590fd6848
Steven McGrath <smcgrath@tenable.com>
2013-03-12 12:05:24 -0500

    Added/modified setup.py files as needed.

--- client/setup.py
+++ client/setup.py
@@ -1,31 +1,34 @@
 from distutils.core import setup
+import sys
 
 setup(
-    name='DoFler Service',
-    version='0.2.99',
+    name='DoFler',
+    version='0.1.2',
     description='Dashboard of Fail - A distibuted Wall of Sheep',
     author='Steven McGrath',
     author_email='steve@chigeek.com',
     url='https://github.com/SteveMcGrath/DoFler',
-    py_modules=['doflersvc',],
+    packages=['dofler', 'dofler.monitor', 'dofler.api'],
     entry_points={
         'console_scripts': [
-            'dofler-server = doflersvc:start',
+            'dofler-client = dofler.svc:client',
+            'dofler-server = dofler.svc:server',
             ]
     },
     install_requires=[
-        'bottle', 
-        'pymongo',
-        'bleach',
-        'paste',
+        'beautifulsoup',
+        'pexpect',
+        'MultipartPostHandler',
     ],
     data_files=[
-        ('/etc/dofler', ['service.conf']),
-        ('/usr/share/dofler', ['static/jquery.js', 'static/dofler.js',
+        ('/etc', ['dofler.config']),
+        ('/etc/init', ['init-scripts/dofler-client.conf', 
+                       'init-scripts/dofler-client.conf']),
+        ('/usr/share/dofler', ['static/jquery.js', 'static/live-view.js',
                                'static/style.css', 'static/viewer.html'])
     ],
     classifiers=[
-        'Development Status :: 4 - Beta',
+        'Development Status :: 3 - Alpha',
         'Intended Audience :: Information Technology',
         'License :: OSI Approved :: GNU Lesser General Public License v2 (LGPLv2)',
         'Natural Language :: English',
