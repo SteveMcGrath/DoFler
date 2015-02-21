6e03a9f4679e0a0d747f6e33d4f334d58b09d1d0
Steven McGrath <steve@chigeek.com>
2015-02-21 12:32:51 -0600

    BETA 5 bitches!

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='DoFler',
-    version='0.4.2.0b3',
+    version='0.4.2.0b5',
     description='Dashboard of Fail',
     author='Steven McGrath',
     author_email='steve@chigeek.com',
