1db4cb8fed32369859a50c4fd562316bb7f04370
David Marby <david@dmarby.se>
2017-04-24 21:10:17 +0200

    Initial container setup

--- bin/pvs_anon.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-import sys
-import re
-
-pvs_anon = open(sys.argv[1] + '.ANON.html', 'w')
-with open(sys.argv[1]) as pvs_original:
-    pvs_anon.write(re.sub(r'\.\d{1,3}\.\d{1,3} \(', '.XXX.XXX (', 
-                          pvs_original.read()))
-pvs_anon.close()
