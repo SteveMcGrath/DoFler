8ab3575064d88fa626619c8100d7490a6bf82828
David Marby <david@dmarby.se>
2017-04-29 04:37:53 +0200

    Automatically attach/detach the network interface to the span container

--- containers/network_manager/span.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-tail -f /dev/null
-#export DOCKER_HOST=${DOCKER_HOST:-"unix:///docker.sock"}_test_docker ()
-
-#if ! docker -D info; then
-#  echo "Error: couldn't connect to $DOCKER_HOST"
-#  exit 1
-#fi
